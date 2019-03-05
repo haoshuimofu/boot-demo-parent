d4eda166da6e7cf1d568ab7edfa21a5b9055088d
100ME\wude <wude@100.me>
2019-03-05 13:39:01 +0800

    Guava Cache测试

---
+++ boot-demo-test/src/main/java/guava/cache/Student.java
@@ -0,0 +1,27 @@
+package guava.cache;
+
+/**
+ * @Author ddmc
+ * @Create 2019-03-05 11:04
+ */
+public class Student {
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
