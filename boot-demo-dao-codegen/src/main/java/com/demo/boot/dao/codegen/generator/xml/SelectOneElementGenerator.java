9829de156339a9bccd445e7e4cfa198d0af556f9
100ME\wude <wude@100.me>
2019-03-24 15:41:51 +0800

    MBG代码修改

--- boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/generator/xml/SelectOneElementGenerator.java
+++ boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/generator/xml/SelectOneElementGenerator.java
@@ -2,7 +2,6 @@ package com.demo.boot.dao.codegen.generator.xml;
 
 import com.demo.boot.dao.codegen.contants.MybatisGeneratorConstants;
 import org.mybatis.generator.api.IntrospectedColumn;
-import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
@@ -12,7 +11,9 @@ import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElem
 import java.util.List;
 
 /**
- * selectOne方法sql语句生成器
+ * <p>selectOne方法sqlMap生成器</p>
+ * <p>如果数据库表为单列主键，查询where条件忽略主键，可以根据selectById直接查询，但是如果没有非主键列就不生成sqlMap了</p>
+ * <p>如果数据库表没有主键列或主键列数大于1在sql语句生成好后追加limit 1</p>
  *
  * @Author wude
  * @Create 2017-06-14 11:03
@@ -22,34 +23,34 @@ public class SelectOneElementGenerator extends AbstractXmlElementGenerator {
     @Override
     public void addElements(XmlElement parentElement) {
 
+        if (introspectedTable.getPrimaryKeyColumns().size() == 1 && introspectedTable.getNonPrimaryKeyColumns().size() == 0) {
+            return;
+        }
+
         XmlElement answer = new XmlElement("select");
         answer.addAttribute(new Attribute("id", "selectOne"));
-        // 参数类型
-        FullyQualifiedJavaType parameterType = new FullyQualifiedJavaType(this.introspectedTable.getBaseRecordType());
-        answer.addAttribute(new Attribute("parameterType", parameterType.getFullyQualifiedName()));
-        // 添加ResultMap
+        // parameterType就是Model类
+        // FullyQualifiedJavaType parameterType = new FullyQualifiedJavaType(this.introspectedTable.getBaseRecordType());
+        // answer.addAttribute(new Attribute("parameterType", parameterType.getFullyQualifiedName()));
+        answer.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
+        // 追加resultMap=BaseResultMap
         answer.addAttribute(new Attribute("resultMap", "BaseResultMap"));
-        // 标签内第一行添加mbg.generated注解
+        // @mbg.generated
         context.getCommentGenerator().addComment(answer);
-        // select语句
+
         StringBuilder selectClause = new StringBuilder();
         selectClause.append("SELECT ")
                 .append(MybatisGeneratorConstants.XML_ELEMENT_BASE_COLUMN_LIST)
-                .append(" FROM ");
-        selectClause.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
+                .append(" FROM ")
+                .append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
         answer.addElement(new TextElement(selectClause.toString()));
 
         XmlElement whereElement = new XmlElement("where");
-        // 如果Table主键有且只有一列, 建议查询条件中不要带主键列, 这种情况下可以直接调用selectById方法
-        // 当Table存在复合主键甚至不存在主键, 查询条件包括所有列
-        List<IntrospectedColumn> columns = introspectedTable.getAllColumns();
-        if (introspectedTable.getPrimaryKeyColumns().size() == 1) {
-            columns = introspectedTable.getNonPrimaryKeyColumns();
-        }
-        // 为每个Column添加ifNotNull判断条件
+        // 如果数据库表为单列主键，where条件就不包括主键了，否则where条件包括所有列
+        List<IntrospectedColumn> columns = introspectedTable.getPrimaryKeyColumns().size() == 1 ? introspectedTable.getNonPrimaryKeyColumns() : introspectedTable.getAllColumns();
         for (IntrospectedColumn column : columns) {
-            selectClause.setLength(0);
             XmlElement ifElement = new XmlElement("if");
+            selectClause.setLength(0);
             selectClause.append(column.getJavaProperty());
             selectClause.append(" != null");
             ifElement.addAttribute(new Attribute("test", selectClause.toString()));
--- boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/generator/xml/UpdateElementGenerator.java
+++ boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/generator/xml/UpdateElementGenerator.java
@@ -11,7 +11,10 @@ import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElem
 import java.util.List;
 
 /**
- * update方法sql语句生成器
+ * <p>update方法sqlMap生成器</p>
+ * <p>入参就是Model类，where条件就是主键列，如果数据库表<em>没有主键列</em>就不生成sqlMap了</p>
+ * <p>Model类对应数据库表主键有多列，where后面条件会有多个，用and拼接</p>
+ * <p>因为update sql语句主要set的是非主键列，所以数据库表<em>没有非主键列</em>也不生成sqlMap了</p>
  *
  * @Author wude
  * @Create 2017-06-15 13:26
@@ -29,9 +32,9 @@ public class UpdateElementGenerator extends AbstractXmlElementGenerator {
         }
         XmlElement answer = new XmlElement("update");
         answer.addAttribute(new Attribute("id", "update"));
-        // parameterType就是Model类
+        // parameterType就是数据表对应的Model类
         answer.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
-        // 标签内第一行生成mbg.generated注解
+        // @mbg.generated 注释添加
         this.context.getCommentGenerator().addComment(answer);
 
         StringBuilder updateClause = new StringBuilder();
@@ -43,11 +46,13 @@ public class UpdateElementGenerator extends AbstractXmlElementGenerator {
         XmlElement setElement = new XmlElement("set");
         answer.addElement(setElement);
         for (IntrospectedColumn column : columns) {
+            // 为if标签添加属性：test="xx != null"
             XmlElement ifElement = new XmlElement("if");
             updateClause.setLength(0);
             updateClause.append(column.getJavaProperty());
             updateClause.append(" != null");
             ifElement.addAttribute(new Attribute("test", updateClause.toString()));
+            // 在if标签内添加 xx = #{xx,jdbcType=xx}
             updateClause.setLength(0);
             updateClause.append(MyBatis3FormattingUtilities.getAliasedEscapedColumnName(column));
             updateClause.append(" = ");
