baedbf4c6ed54b40b92eebb50f850a825d02fe26
100ME\wude <wude@100.me>
2019-03-24 16:34:21 +0800

    MBG代码修改

--- boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/generator/xml/SelectAllElementGenerator.java
+++ boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/generator/xml/SelectAllElementGenerator.java
@@ -7,7 +7,7 @@ import org.mybatis.generator.api.dom.xml.XmlElement;
 import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElementGenerator;
 
 /**
- * selectAll方法sql语句生成器
+ * <p>selectAll方法sqlMap生成器</p>
  *
  * @Author wude
  * @Create 2017-06-14 13:11
@@ -22,14 +22,14 @@ public class SelectAllElementGenerator extends AbstractXmlElementGenerator {
     public void addElements(XmlElement parentElement) {
         XmlElement answer = new XmlElement("select");
         answer.addAttribute(new Attribute("id", "selectAll"));
-        // 添加ResultMap
         answer.addAttribute(new Attribute("resultMap", "BaseResultMap"));
-        // 标签内第一行生成mbg.generated注解
+        // @mbg.generated
         context.getCommentGenerator().addComment(answer);
-
         StringBuilder selectClause = new StringBuilder();
-        selectClause.append("SELECT ").append(MybatisGeneratorConstants.XML_ELEMENT_BASE_COLUMN_LIST).append(" FROM ");
-        selectClause.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
+        selectClause.append("SELECT ")
+                .append(MybatisGeneratorConstants.XML_ELEMENT_BASE_COLUMN_LIST)
+                .append(" FROM ")
+                .append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
         answer.addElement(new TextElement(selectClause.toString()));
         parentElement.addElement(answer);
     }
