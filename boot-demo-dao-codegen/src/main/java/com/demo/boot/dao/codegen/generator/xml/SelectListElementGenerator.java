91bf2490dd81e3186ec5932f4adebfaf3fb548b1
100ME\wude <wude@100.me>
2019-03-24 16:14:41 +0800

    MBG代码修改

--- boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/generator/xml/SelectListElementGenerator.java
+++ boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/generator/xml/SelectListElementGenerator.java
@@ -12,7 +12,8 @@ import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElem
 import java.util.List;
 
 /**
- * selectList方法sql语句生成器
+ * <p>selectList方法sqlMap生成器</p>
+ * <p>如果数据库表为单列主键，查询where条件忽略主键，可以根据selectById直接查询，但是如果没有非主键列就不生成sqlMap了</p>
  *
  * @Author wude
  * @Create 2017-06-14 13:06
@@ -24,39 +25,37 @@ public class SelectListElementGenerator extends AbstractXmlElementGenerator {
 
         XmlElement answer = new XmlElement("select");
         answer.addAttribute(new Attribute("id", "selectList"));
-        // 参数类型
+        // parameterType就是Model类
         FullyQualifiedJavaType parameterType = new FullyQualifiedJavaType(this.introspectedTable.getBaseRecordType());
         answer.addAttribute(new Attribute("parameterType", parameterType.getFullyQualifiedName()));
-        // 添加ResultMap
+        // resultMap=BaseResultMap
         answer.addAttribute(new Attribute("resultMap", "BaseResultMap"));
-        // 标签内第一行添加mbg.generated注解
+        // @mbg.generated
         context.getCommentGenerator().addComment(answer);
 
         StringBuilder selectClause = new StringBuilder();
-        selectClause.append("SELECT ").append(MybatisGeneratorConstants.XML_ELEMENT_BASE_COLUMN_LIST).append(" FROM ");
-        selectClause.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
+        selectClause.append("SELECT ")
+                .append(MybatisGeneratorConstants.XML_ELEMENT_BASE_COLUMN_LIST)
+                .append(" FROM ")
+                .append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
         answer.addElement(new TextElement(selectClause.toString()));
 
+        // 如果数据库表为单列主键，where条件就不包括主键了，否则where条件包括所有列
+        List<IntrospectedColumn> columns = introspectedTable.getPrimaryKeyColumns().size() == 1 ? introspectedTable.getNonPrimaryKeyColumns() : introspectedTable.getAllColumns();
         XmlElement whereElement = new XmlElement("where");
-        // 如果Table主键有且只有一列, 建议查询条件中不要带主键列, 这种情况下可以直接调用selectById方法
-        // 当Table存在复合主键甚至不存在主键, 查询条件包括所有列
-        List<IntrospectedColumn> columns = introspectedTable.getAllColumns();
-        if (introspectedTable.getPrimaryKeyColumns().size() == 1) {
-            columns = introspectedTable.getNonPrimaryKeyColumns();
-        }
         // 为每个Column添加ifNotNull判断条件
-        for (IntrospectedColumn introspectedColumn : columns) {
-            selectClause.setLength(0);
+        for (IntrospectedColumn column : columns) {
             XmlElement ifElement = new XmlElement("if");
-            selectClause.append(introspectedColumn.getJavaProperty());
+            selectClause.setLength(0);
+            selectClause.append(column.getJavaProperty());
             selectClause.append(" != null");
             ifElement.addAttribute(new Attribute("test", selectClause.toString()));
 
             selectClause.setLength(0);
             selectClause.append("AND ");
-            selectClause.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+            selectClause.append(MyBatis3FormattingUtilities.getEscapedColumnName(column));
             selectClause.append(" = ");
-            selectClause.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn, ""));
+            selectClause.append(MyBatis3FormattingUtilities.getParameterClause(column, ""));
             ifElement.addElement(new TextElement(selectClause.toString()));
             whereElement.addElement(ifElement);
         }
