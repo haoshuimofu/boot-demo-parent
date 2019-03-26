6285e284b83fa5cc48621489743ea56c031c77cc
100ME\wude <wude@100.me>
2019-03-26 16:12:57 +0800

    code fixed

--- demo.sql
+++ demo.sql
@@ -1,37 +1,52 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : local
-Source Server Version : 50163
-Source Host           : localhost:3306
-Source Database       : demo
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50163
+ Source Host           : localhost:3306
+ Source Schema         : demo
 
-Target Server Type    : MYSQL
-Target Server Version : 50163
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50163
+ File Encoding         : 65001
 
-Date: 2019-03-18 23:47:22
+ Date: 26/03/2019 16:11:57
 */
 
-SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for `t_users`
--- ----------------------------
-DROP TABLE IF EXISTS `t_users`;
-CREATE TABLE `t_users` (
-  `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '自增长主键',
-  `name` varchar(40) NOT NULL,
-  `register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `age` int(3) NOT NULL DEFAULT '0',
-  `field1` varchar(20) NOT NULL DEFAULT '',
-  `field2` varchar(20) DEFAULT '',
-  `field3` varchar(20) DEFAULT NULL,
-  `field4` varchar(20) DEFAULT NULL,
-  `field5` varchar(20) DEFAULT NULL,
-  `field6` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Records of t_users
+-- Table structure for t_roles
 -- ----------------------------
+DROP TABLE IF EXISTS `t_roles`;
+CREATE TABLE `t_roles`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增长主键',
+  `role_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `role_desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for t_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_role`;
+CREATE TABLE `t_user_role`  (
+  `user_id` int(11) NOT NULL COMMENT '户用ID',
+  `role_id` int(11) NOT NULL COMMENT '角色ID',
+  PRIMARY KEY (`user_id`, `role_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for t_users
+-- ----------------------------
+DROP TABLE IF EXISTS `t_users`;
+CREATE TABLE `t_users`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增长主键',
+  `username` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
