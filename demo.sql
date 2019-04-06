f427a430e820c2999bf2a6334cff99eb5867a94f
wude <279759300@qq.com>
2019-04-06 17:24:04 +0800

    夜晚利于思考

--- demo.sql
+++ demo.sql
@@ -1,52 +1,71 @@
 /*
- Navicat Premium Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : local
- Source Server Type    : MySQL
- Source Server Version : 50163
- Source Host           : localhost:3306
- Source Schema         : demo
+Source Server         : local
+Source Server Version : 50163
+Source Host           : localhost:3306
+Source Database       : demo
 
- Target Server Type    : MySQL
- Target Server Version : 50163
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 50163
+File Encoding         : 65001
 
- Date: 26/03/2019 16:11:57
+Date: 2019-04-06 17:23:24
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `store_product_batch_monitor`
+-- ----------------------------
+DROP TABLE IF EXISTS `store_product_batch_monitor`;
+CREATE TABLE `store_product_batch_monitor` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `store_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `batch_id` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `test_index` (`store_id`,`product_id`,`batch_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for t_roles
+-- Records of store_product_batch_monitor
 -- ----------------------------
-DROP TABLE IF EXISTS `t_roles`;
-CREATE TABLE `t_roles`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增长主键',
-  `role_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `role_desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Table structure for t_user_role
+-- Table structure for `t_test`
 -- ----------------------------
-DROP TABLE IF EXISTS `t_user_role`;
-CREATE TABLE `t_user_role`  (
-  `user_id` int(11) NOT NULL COMMENT '户用ID',
-  `role_id` int(11) NOT NULL COMMENT '角色ID',
-  PRIMARY KEY (`user_id`, `role_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+DROP TABLE IF EXISTS `t_test`;
+CREATE TABLE `t_test` (
+  `id_first` int(11) NOT NULL DEFAULT '0',
+  `id_two` int(11) NOT NULL DEFAULT '0',
+  `gender` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_first`,`id_two`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for t_users
+-- Records of t_test
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_users`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_users`;
-CREATE TABLE `t_users`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增长主键',
-  `username` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
-  `password` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `t_users` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '自增长主键',
+  `name` varchar(40) NOT NULL,
+  `register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `age` int(3) NOT NULL DEFAULT '0',
+  `description` text,
+  `field1` varchar(20) NOT NULL DEFAULT '',
+  `field2` varchar(20) DEFAULT '',
+  `field3` varchar(20) DEFAULT NULL,
+  `field4` varchar(20) DEFAULT NULL,
+  `field5` varchar(20) DEFAULT NULL,
+  `field6` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- ----------------------------
+-- Records of t_users
+-- ----------------------------
