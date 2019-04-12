df3a510718a916ff71348c8edb799e7ff91663d6
100ME\wude <wude@100.me>
2019-04-12 10:28:54 +0800

    code commit

--- boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/DemoMybatisGenerator1.java
+++ boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/DemoMybatisGenerator1.java
@@ -2,28 +2,27 @@ package com.demo.boot.dao.codegen;
 
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.api.ProgressCallback;
+import org.mybatis.generator.api.VerboseProgressCallback;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
-import org.mybatis.generator.internal.NullProgressCallback;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Mybatis Generator java调用入口
- *
- * @Author wude
- * @Create 2017-05-11 10:24
+ * @Author ddmc
+ * @Create 2019-04-12 10:27
  */
-public class DemoMybatisGenerator {
+public class DemoMybatisGenerator1 {
 
     private static final String USER_MODULE_CONFIG = "classpath:user/generatorConfig.xml";
     private static final String BUSINESS_MODULE_CONFIG = "classpath:business/generatorConfig.xml";
@@ -34,32 +33,28 @@ public class DemoMybatisGenerator {
 
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
-//        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("user.generatorConfig.xml");
+        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("user.generatorConfig.xml");
         File configFile = ResourceUtils.getFile(KANBAN_MODULE_CONFIG);
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = null;
         try {
-            config = cp.parseConfiguration(configFile);
-//            config = cp.parseConfiguration(inputStream);
+            config = cp.parseConfiguration(inputStream);
             DefaultShellCallback callback = new DefaultShellCallback(overwrite);
             MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
 
-            ProgressCallback progressCallback = new NullProgressCallback();
-            // progressCallback = new VerboseProgressCallback(); // 详细处理回调
+            ProgressCallback progressCallback = new VerboseProgressCallback();
             myBatisGenerator.generate(progressCallback);
-//            myBatisGenerator.generate(null);
         } catch (IOException | XMLParserException | InvalidConfigurationException | InterruptedException | SQLException e) {
             e.printStackTrace();
         } finally {
-//            if (inputStream != null) {
-//                try {
-//                    inputStream.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 //            }
+            }
         }
-
     }
-
 }
