97a682f01b79d6e122f28211648efce252869cd4
100ME\wude <wude@100.me>
2019-04-22 11:54:16 +0800

    code fixed

--- boot-demo-core/src/main/java/com/demo/boot/redis/RedisClusterAutoConfiguration.java
+++ boot-demo-core/src/main/java/com/demo/boot/redis/RedisClusterAutoConfiguration.java
@@ -8,18 +8,16 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import redis.clients.jedis.HostAndPort;
-import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPoolConfig;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * @Author ddmc
+ * RedisCluster配置
+ *
+ * @Author wude
  * @Create 2019-04-17 18:07
  */
 @Configuration
@@ -35,10 +33,9 @@ public class RedisClusterAutoConfiguration {
     public RedisClusterConfiguration redisClusterConfiguration(RedisClusterConfigurationProperties redisClusterConfigurationProperties) {
         RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
         List<RedisNode> redisNodes = Stream.of(redisClusterConfigurationProperties.getClusterNodes().split(",")).map(hostAndPort -> {
-                    String[] redisNodeInfo = hostAndPort.split(":");
-                    RedisNode redisNode = new RedisNode(redisNodeInfo[0], Integer.parseInt(redisNodeInfo[1]));
-                    return redisNode;
-                }).collect(Collectors.toList());
+            String[] redisNodeInfo = hostAndPort.split(":");
+            return new RedisNode(redisNodeInfo[0], Integer.parseInt(redisNodeInfo[1]));
+        }).collect(Collectors.toList());
         redisClusterConfiguration.setClusterNodes(redisNodes);
         redisClusterConfiguration.setMaxRedirects(Integer.parseInt(redisClusterConfigurationProperties.getMaxRedirects()));
         return redisClusterConfiguration;
@@ -47,14 +44,16 @@ public class RedisClusterAutoConfiguration {
     public @Bean
     RedisConnectionFactory connectionFactory(RedisClusterConfiguration redisClusterConfiguration) {
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setMaxTotal(100);
-        jedisPoolConfig.setMaxIdle(500);
-        jedisPoolConfig.setMinIdle(300);
+        jedisPoolConfig.setMaxTotal(1000);
+        jedisPoolConfig.setMaxIdle(100);
+        jedisPoolConfig.setMinIdle(10);
+        // the maximum number of "active" objects has been reached
         jedisPoolConfig.setBlockWhenExhausted(true);
         jedisPoolConfig.setMaxWaitMillis(6000);
         jedisPoolConfig.setTestOnBorrow(true);
+        jedisPoolConfig.setTestOnReturn(false);
 
-        return new JedisConnectionFactory(redisClusterConfiguration,jedisPoolConfig);
+        return new JedisConnectionFactory(redisClusterConfiguration, jedisPoolConfig);
     }
 
     public @Bean
@@ -63,25 +62,5 @@ public class RedisClusterAutoConfiguration {
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
-
-//    public @Bean
-//    RedisConnectionFactory connectionFactory() {
-//
-//        return new JedisConnectionFactory(
-//                new RedisClusterConfiguration(clusterProperties.getNodes()));
-//    }
-
-//    @Bean
-//    public JedisCluster getJedisCluster(RedisClusterConfigurationProperties redisClusterConfigurationProperties) {
-//        String[] serverArray = redisClusterConfigurationProperties.getClusterNodes().split(",");
-//        Set<HostAndPort> nodes = new HashSet<>();
-//
-//        for (String ipPort : serverArray) {
-//            String[] ipPortPair = ipPort.split(":");
-//            nodes.add(new HostAndPort(ipPortPair[0].trim(), Integer.valueOf(ipPortPair[1].trim())));
-//        }
-//        return new JedisCluster(nodes, redisClusterConfigurationProperties.getCommandTimeout());
-//    }
-
 
 }
