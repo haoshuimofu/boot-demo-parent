7dd67367613015fefea26fd96193042650522c34
100ME\wude <wude@100.me>
2019-04-22 11:55:35 +0800

    code fixed

--- boot-demo-web/src/main/java/com/demo/boot/web/controller/RedisClusterController.java
+++ boot-demo-web/src/main/java/com/demo/boot/web/controller/RedisClusterController.java
@@ -1,14 +1,13 @@
 package com.demo.boot.web.controller;
 
 import com.demo.boot.BootDemoWebApplication;
-import com.demo.boot.redis.RedisClusterConfigurationProperties;
+import com.demo.boot.user.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import redis.clients.jedis.JedisCluster;
 
 /**
  * @Author ddmc
@@ -18,27 +17,17 @@ import redis.clients.jedis.JedisCluster;
 @SpringBootTest(classes = {BootDemoWebApplication.class, RedisClusterController.class})
 public class RedisClusterController {
 
-//    @Autowired
-//    private JedisCluster jedisCluster;
-//    @Autowired
-//    RedisClusterConfigurationProperties redisClusterConfigurationProperties;
 
     @Autowired
     private RedisTemplate redisTemplate;
 
     @Test
     public void testSetGet() {
-//        String[] serverArray = redisClusterConfigurationProperties.getClusterNodes().split(",");
-//        Set<HostAndPort> nodes = new HashSet<>();
-//
-//        for (String ipPort : serverArray) {
-//            String[] ipPortPair = ipPort.split(":");
-//            nodes.add(new HostAndPort(ipPortPair[0].trim(), Integer.valueOf(ipPortPair[1].trim())));
-//        }
-//        JedisCluster jedisCluster = new JedisCluster(nodes, redisClusterConfigurationProperties.getCommandTimeout());
-//        jedisCluster.set("name", "wude");
-//        System.out.println(jedisCluster.get("name"));
-        redisTemplate.opsForValue().set("name", "wude");
+        User user = new User();
+        user.setId(1L);
+        user.setName("Wude");
+
+        redisTemplate.opsForValue().set("name", user);
         System.out.println(redisTemplate.opsForValue().get("name"));
 
 
