4b231769f7d7eb51753ede541ff06555ddd95eaa
100ME\wude <wude@100.me>
2019-04-25 17:10:36 +0800

    RedisTemplate Value加序列，要不然不能存对象

--- boot-demo-web/src/main/java/com/demo/boot/web/controller/RedisClusterController.java
+++ boot-demo-web/src/main/java/com/demo/boot/web/controller/RedisClusterController.java
@@ -1,13 +1,16 @@
 package com.demo.boot.web.controller;
 
 import com.demo.boot.BootDemoWebApplication;
-import com.demo.boot.user.model.User;
+import com.demo.boot.redis.RedisCacheManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 /**
  * @Author ddmc
@@ -17,19 +20,35 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest(classes = {BootDemoWebApplication.class, RedisClusterController.class})
 public class RedisClusterController {
 
+    Logger logger = LoggerFactory.getLogger(RedisClusterController.class);
+
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisCacheManager redisCacheManager;
 
     @Test
     public void testSetGet() {
-        User user = new User();
-        user.setId(1L);
-        user.setName("Wude");
+        boolean result = redisCacheManager.setnx("ip", "10.172.73.63", 10);
+        logger.info("put first result: {}", result);
+        result = redisCacheManager.setnx("ip", "10.172.73.63", 10);
+        logger.info("put second result: {}", result);
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        result = redisCacheManager.setnx("ip", "10.172.73.63", 10);
+        logger.info("put third result: {}", result);
+    }
 
-        redisTemplate.opsForValue().set("name", user);
-        System.out.println(redisTemplate.opsForValue().get("name"));
-
-
+    @Test
+    public void testList() {
+        redisCacheManager.del("list");
+        Long result = redisCacheManager.putList("list", new Integer[]{1, 2, 3, 4, 5});
+        ((List<Integer>) redisCacheManager.get("list")).forEach(System.out::print);
+        redisCacheManager.rightPop("list");
+        System.out.println();
+        ((List<Integer>) redisCacheManager.get("list")).forEach(System.out::print);
+        System.out.println();
     }
 }
