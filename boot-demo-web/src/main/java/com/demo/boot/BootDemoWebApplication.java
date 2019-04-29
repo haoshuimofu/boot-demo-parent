7c3e684b8089dcaf8dbc88013593ae72781b5ebe
100ME\wude <wude@100.me>
2019-04-29 16:45:25 +0800

    @Log 搞一下日志拦截

--- boot-demo-web/src/main/java/com/demo/boot/BootDemoWebApplication.java
+++ boot-demo-web/src/main/java/com/demo/boot/BootDemoWebApplication.java
@@ -1,5 +1,6 @@
 package com.demo.boot;
 
+import com.demo.starter.log.Log;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.Map;
 
 @SpringBootApplication(scanBasePackages = "com.demo.boot")
 @EnableTransactionManagement // 开启事务管理
@@ -22,6 +28,20 @@ public class BootDemoWebApplication extends SpringBootServletInitializer {
         ApplicationContext ctx = SpringApplication.run(BootDemoWebApplication.class, args);
         // spring.profiles.active
         logger.info("Spring.profile.active: {}", StringUtils.join(ctx.getEnvironment().getActiveProfiles(), ", "));
+
+        RequestMappingHandlerMapping handlerMapping = ctx.getBean(RequestMappingHandlerMapping.class);
+        for (Map.Entry<RequestMappingInfo, HandlerMethod> entry : handlerMapping.getHandlerMethods().entrySet()) {
+            RequestMappingInfo requestMappingInfo = entry.getKey();
+            HandlerMethod handlerMethod = entry.getValue();
+
+            Log log = handlerMethod.getMethod().getDeclaringClass().getAnnotation(Log.class);
+            if (log == null) {
+                log = handlerMethod.getMethod().getAnnotation(Log.class);
+            }
+            if (log != null) {
+                System.out.println(String.format("%s -> %s", requestMappingInfo.getPatternsCondition().getPatterns().iterator().next(), handlerMethod.getMethod().getName()));
+            }
+        }
     }
 
     /**
