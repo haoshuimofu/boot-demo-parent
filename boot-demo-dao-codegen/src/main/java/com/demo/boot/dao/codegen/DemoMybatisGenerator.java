5406dca5d0f25d9ff9ea2af14e62528be095d639
100ME\wude <wude@100.me>
2019-05-06 11:24:34 +0800

    codegen

--- boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/DemoMybatisGenerator.java
+++ boot-demo-dao-codegen/src/main/java/com/demo/boot/dao/codegen/DemoMybatisGenerator.java
@@ -36,7 +36,7 @@ public class DemoMybatisGenerator {
 
         List<String> warnings = new ArrayList<>();
         boolean overwrite = true;
-        File configFile = ResourceUtils.getFile(PRIVILEGE_MODULE_CONFIG);
+        File configFile = ResourceUtils.getFile(KANBAN_MODULE_CONFIG);
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = null;
         try {
